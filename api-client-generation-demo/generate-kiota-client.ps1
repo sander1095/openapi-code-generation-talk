8774a9cc169c28ff0b5c9243479744506a5fe739
Sander ten Brinke <s.tenbrinke2@gmail.com>
2024-01-13 22:59:27 +0100

    Generate kiota client

--- api-client-generation-demo/generate-kiota-client.ps1
+++ api-client-generation-demo/generate-kiota-client.ps1
@@ -1,5 +1,6 @@
-dotnet kiota generate -l csharp `
-					  --openapi "conference_api.json" `
-					  -o .\ConferenceApp\Clients\Kiota `
+dotnet kiota generate --clean-output `
+					  --language csharp `
+					  --openapi "https://localhost:7135/swagger/v1/swagger.json" `
+					  -o ConferenceApp/Clients/Kiota `
 					  -n ConferenceApp.Clients.Kiota `
 					  -c KiotaConferenceClient
