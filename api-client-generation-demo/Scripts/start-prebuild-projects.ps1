ce36a001f949967e44e62c7df5d6aa99097f4b0f
Sander ten Brinke <s.tenbrinke2@gmail.com>
2024-01-17 21:28:22 +0100

    More scripts and better code

---
+++ api-client-generation-demo/Scripts/start-prebuild-projects.ps1
@@ -0,0 +1,3 @@
+start ../Prebuild/minimal-api/ConferenceServerMinimalAPI.exe
+start ../Prebuild/controllers/ConferenceServerControllers.exe
+start ../Prebuild/client-app/ConferenceApp.exe
--- api-client-generation-demo/generate-nswag-client.ps1
+++
@@ -1,1 +0,0 @@
-dotnet nswag run conference.nswag
