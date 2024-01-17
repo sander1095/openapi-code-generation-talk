dc37babecad2ed7d2340b0496217632218c72f29
Sander ten Brinke <s.tenbrinke2@gmail.com>
2024-01-17 20:56:28 +0100

    Move prebuild to correct folder

--- api-client-generation-demo/generate-prebuild-projects.ps1
+++ api-client-generation-demo/generate-prebuild-projects.ps1
@@ -1,3 +1,3 @@
-dotnet publish ConferenceApp/ -r win-x64 -o ../Prebuild/client-app
-dotnet publish ConferenceServerControllers/ -r win-x64 -o ../Prebuild/controllers
-dotnet publish ConferenceServerMinimalAPI -r win-x64 -o ../Prebuild/minimal-api
+dotnet publish ConferenceApp/ -r win-x64 -o Prebuild/client-app
+dotnet publish ConferenceServerControllers/ -r win-x64 -o Prebuild/controllers
+dotnet publish ConferenceServerMinimalAPI -r win-x64 -o Prebuild/minimal-api
